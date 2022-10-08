d8ee98d081754f864735544390b7c773c31723df
Jonathan Hiles <7577851+axieum@users.noreply.github.com>
2022-10-09 00:12:58 +1000

    fix: unable to join servers with chat verification turned on (fixes #60) (#66)

--- src/main/java/me/axieum/mcmod/authme/mixin/MinecraftClientAccessor.java
+++ src/main/java/me/axieum/mcmod/authme/mixin/MinecraftClientAccessor.java
@@ -4,7 +4,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import com.mojang.authlib.minecraft.UserApiService;
+
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.SocialInteractionsManager;
+import net.minecraft.client.report.AbuseReportContext;
+import net.minecraft.client.util.ProfileKeys;
 import net.minecraft.client.util.Session;
 
 /**
@@ -21,4 +26,40 @@ public interface MinecraftClientAccessor
     @Accessor
     @Mutable
     void setSession(Session session);
+
+    /**
+     * Sets the Minecraft user API service.
+     *
+     * @param userApiService new Minecraft user API service
+     */
+    @Accessor
+    @Mutable
+    void setUserApiService(UserApiService userApiService);
+
+    /**
+     * Sets the Minecraft social interactions manager.
+     *
+     * @param socialInteractionsManager new Minecraft social interactions manager
+     */
+    @Accessor
+    @Mutable
+    void setSocialInteractionsManager(SocialInteractionsManager socialInteractionsManager);
+
+    /**
+     * Sets the Minecraft profile keys.
+     *
+     * @param profileKeys new Minecraft profile keys
+     */
+    @Accessor
+    @Mutable
+    void setProfileKeys(ProfileKeys profileKeys);
+
+    /**
+     * Sets the Minecraft abuse report context.
+     *
+     * @param abuseReportContext new Minecraft abuse report context
+     */
+    @Accessor
+    @Mutable
+    void setAbuseReportContext(AbuseReportContext abuseReportContext);
 }
